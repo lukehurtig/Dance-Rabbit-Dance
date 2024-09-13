6e36c8bf396610a1bd368a3443d80a4c6d073703
Luke Hurtig <lhurtig@outlook.com>
2024-09-13 16:02:04 -0500

    Got the initialization and content loading done

--- Dance Rabbit Dance/Game1.cs
+++ Dance Rabbit Dance/Game1.cs
@@ -16,7 +16,6 @@ namespace Dance_Rabbit_Dance
         private Wabbit wabbit = new Wabbit();
         private Dancer dancer = new Dancer();
         private Dancer dancer1 = new Dancer();
-        private Dancer dancer2 = new Dancer();
         private SpriteFont _font;
         private int score = 0;
 
@@ -32,30 +31,31 @@ namespace Dance_Rabbit_Dance
             // TODO: Add your initialization logic here
             arrows = new List<Arrow>()
             {
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/4) / 2), VIEWPORT_WIDTH - 60), Direction.Right, true),
-                new Arrow(new Vector2((VIEWPORT_WIDTH/4), VIEWPORT_WIDTH - 60), Direction.Up, true),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/2)), VIEWPORT_WIDTH - 60), Direction.Left, true),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/1.5f)), VIEWPORT_WIDTH - 60), Direction.Down, true),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/4) / 2), -40), Direction.Right, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/4) / 2), -40), Direction.Right, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/4) / 2), -40), Direction.Right, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/4) / 2), -40), Direction.Right, false),
-                new Arrow(new Vector2((VIEWPORT_WIDTH/4), -40), Direction.Up, false),
-                new Arrow(new Vector2((VIEWPORT_WIDTH/4), -40), Direction.Up, false),
-                new Arrow(new Vector2((VIEWPORT_WIDTH/4), -40), Direction.Up, false),
-                new Arrow(new Vector2((VIEWPORT_WIDTH/4), -40), Direction.Up, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/2)), -40), Direction.Left, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/2)), -40), Direction.Left, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/2)), -40), Direction.Left, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/2)), -40), Direction.Left, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/1.5f)), -40), Direction.Down, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/1.5f)), -40), Direction.Down, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/1.5f)), -40), Direction.Down, false),
-                new Arrow(new Vector2(((VIEWPORT_WIDTH/1.5f)), -40), Direction.Down, false),
+                new Arrow(new Vector2(VIEWPORT_WIDTH / 8, VIEWPORT_WIDTH - 60), Direction.Right, true),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 3 / 8), VIEWPORT_WIDTH - 60), Direction.Up, true),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 5 / 8), VIEWPORT_WIDTH - 60), Direction.Left, true),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 7 / 8), VIEWPORT_WIDTH - 60), Direction.Down, true),
+                new Arrow(new Vector2(VIEWPORT_WIDTH / 8, -40), Direction.Right, false),
+                new Arrow(new Vector2(VIEWPORT_WIDTH / 8, -40), Direction.Right, false),
+                new Arrow(new Vector2(VIEWPORT_WIDTH / 8, -40), Direction.Right, false),
+                new Arrow(new Vector2(VIEWPORT_WIDTH / 8, -40), Direction.Right, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 3 / 8), -40), Direction.Up, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 3 / 8), -40), Direction.Up, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 3 / 8), -40), Direction.Up, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 3 / 8), -40), Direction.Up, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 5 / 8), -40), Direction.Left, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 5 / 8), -40), Direction.Left, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 5 / 8), -40), Direction.Left, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 5 / 8), -40), Direction.Left, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 7 / 8), -40), Direction.Down, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 7 / 8), -40), Direction.Down, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 7 / 8), -40), Direction.Down, false),
+                new Arrow(new Vector2((VIEWPORT_WIDTH * 7 / 8), -40), Direction.Down, false),
             };
 
-            wabbit.Position = new Vector2(VIEWPORT_WIDTH/2, VIEWPORT_HEIGHT/2);
-
+            wabbit.Position = new Vector2(VIEWPORT_WIDTH / 2, VIEWPORT_HEIGHT * 0.66f);
+            dancer.Position = new Vector2(VIEWPORT_WIDTH * 0.25f, VIEWPORT_HEIGHT/2);
+            dancer1.Position = new Vector2(VIEWPORT_WIDTH * 0.75f, VIEWPORT_HEIGHT / 2);
 
             base.Initialize();
         }
@@ -65,7 +65,14 @@ namespace Dance_Rabbit_Dance
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            foreach ( Arrow arrow in arrows )
+            {
+                arrow.LoadContent(Content);
+            }
 
+            wabbit.LoadContent(Content);
+            dancer.LoadContent(Content);
+            dancer1.LoadContent(Content);
         }
 
         protected override void Update(GameTime gameTime)
